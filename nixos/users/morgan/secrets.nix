f8ae7bf31a115d5e7e62f75eba3ec5350df9e6a6
morgan <southernpolyhornet@protonmail.com>
2026-01-03 23:43:14 -0600

    password for steamuser

--- nixos/users/morgan/secrets.nix
+++ nixos/users/morgan/secrets.nix
@@ -7,12 +7,12 @@
 
   # Define secrets (using nested YAML structure)
   # Each secret specifies its own sopsFile to avoid conflicts
-  sops.secrets."password_hash" = {
+  sops.secrets."morgan.password_hash" = {
     sopsFile = ./secrets.yaml;
     neededForUsers = true; # Decrypt early so user password is available
   };
 
   # Use hashedPasswordFile (not hashedPassword) to avoid reading at eval time
   # The file will be created at activation time by SOPS
-  users.users.morgan.hashedPasswordFile = config.sops.secrets."password_hash".path;
+  users.users.morgan.hashedPasswordFile = config.sops.secrets."morgan.password_hash".path;
 }
--- nixos/users/steamuser/secrets.nix
+++ nixos/users/steamuser/secrets.nix
@@ -5,12 +5,13 @@
 
 {
   # Define secrets (using nested YAML structure)
-  sops.secrets."password_hash" = {
+  # Use unique secret name to avoid conflicts with other users
+  sops.secrets."steamuser.password_hash" = {
     sopsFile = ./secrets.yaml;
     neededForUsers = true; # Decrypt early so user password is available
   };
 
   # Use hashedPasswordFile (not hashedPassword) to avoid reading at eval time
   # The file will be created at activation time by SOPS
-  users.users.steamuser.hashedPasswordFile = config.sops.secrets."password_hash".path;
+  users.users.steamuser.hashedPasswordFile = config.sops.secrets."steamuser.password_hash".path;
 }
